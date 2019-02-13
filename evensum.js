8f408e64fccef397f7558832bdbde2179ad760fc
farkasanita <farkasanita@outlook.com>
2019-02-13 16:08:02 +0100

    ossze

--- evensum.js
+++ evensum.js
@@ -1,8 +1,7 @@
-let myMethod = require('./generatearray');
+let myMethod = require('./generateArray');
 let arr = myMethod.generatearray();
 
 let evenSum = (arr) => {
-  console.log(arr);
   let sum = 0;
   for (let i = 0; i < 10; i++) {
     if (arr[i] % 2 === 0) {
@@ -11,5 +10,7 @@ let evenSum = (arr) => {
   }
   return sum;
 };
+
+
 
 module.exports = { evenSum };
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//itt kell dolgozni
+// itt kell dolgozni
+let arr = require('./generateArray');
+let evenS = require('./evensum');
+let oddM = require('./oddModul');
+
+let init = () => {
+  let tomb = arr.generatearray();
+  let num = evenS.evenSum(tomb);
+  console.log(num)
+  let odd = oddM.oddModul(tomb);
+  console.log(odd)
+};
+
+
+init();
